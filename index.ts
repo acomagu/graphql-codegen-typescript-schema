60346514ce2929e8f5fc6280b966e74581d2f6a3
Yuki Ito <acomagu@gmail.com>
2025-07-01 18:23:49 +0900

    schema minification

--- index.ts
+++ index.ts
@@ -9,14 +9,20 @@ const plugin: CodegenPlugin<TypescriptSchemaPluginConfig> = {
   plugin(schema, _documents, config) {
     const { schemaOutput = 'schemaObject' } = config;
 
+    const minifiedSchema = printSchemaWithDirectives(schema)
+      .replace(/#.*/g, '')
+      .replace(/\n/g, ' ')
+      .replace(/\s\s+/g, ' ')
+      .replace(/\s*([{}():,.=@])\s*/g, '$1');
+
     if (schemaOutput === 'schemaObject') {
       return {
         prepend: ['import { buildSchema } from \'graphql\';'],
-        content: `export const schema = buildSchema(${JSON.stringify(printSchemaWithDirectives(schema))});\n`,
+        content: `export const schema = buildSchema(${JSON.stringify(minifiedSchema)});\n`,
       };
     } else if (schemaOutput === 'dslString') {
       return {
-        content: `export const schema = ${JSON.stringify(printSchemaWithDirectives(schema))};\n`,
+        content: `export const schema = ${JSON.stringify(minifiedSchema)};\n`,
       };
     }
 
